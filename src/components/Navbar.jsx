6afb103e97baf0a4517e548ba24152bfefa46766
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-07-29 16:57:02 +0530

    added ui for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Link } from "react-scroll";
+import './css/Navbar.css';
 const Navbar = () => {
   return (
-   <nav className='navbar'>
-      <div className='logo'>
-        CodeSoft
-      </div>
-      <ul className='nav-links'>
-        {["Home", "Courses", "Skills", "Projects", "About", "Contact"].map(item => (
-          <li key={item}>
-            <Link to={item.toLowerCase()} smooth duration={500}>
-              {item}
-            </Link>
-          </li>
-        ))}
+    <nav className="navbar">
+    <div className="navbar-container">
+      <a href="#home" className="logo">Codesoft UI</a>
+      <ul className="nav-links">
+        <li><a href="#courses">Courses</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
-   </nav>
+    </div>
+  </nav>
   )
 }
 
