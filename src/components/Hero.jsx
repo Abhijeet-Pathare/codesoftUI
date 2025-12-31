dd6fe8cd2dbb7c68080e12700cd584516e003fd4
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2025-12-31 12:04:09 +0530

    modified homepage content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,17 @@ const Hero = () => {
   return (
     <section className='hero' id='home'>
       <div className='hero-content'>
-        <h1>Hello, I'm <span className='highlight'>Abhijeet Pathare</span></h1>
+        <h1>नमस्कार, मी तुमचा <span className='highlight'>Tech Guru</span></h1>
         <h2>
           <b>
             <Typewriter
               words={[
-                "Java Backend Developer 💻",
-                "Spring Boot Enthusiast 🌱",
-                "MongoDB & Oracle Expert 🛢️",
-                "DevOps Learner 🚀",
+                "आम्ही शास्त्रशुद्ध पद्धतीने coding शिकवतो.",
+                "Rooted in values, built for the future",
+                // "Java Backend Developer 💻",
+                // "Spring Boot Enthusiast 🌱",
+                // "MongoDB & Oracle Expert 🛢️",
+                // "DevOps Learner 🚀",
               ]}
               loop={0} // 0 = infinite
               cursor
@@ -24,7 +26,7 @@ const Hero = () => {
             />
           </b>
         </h2>
-        <a href="#projects" className='cta-btn'>View My Work</a>
+        {/* <a href="#projects" className='cta-btn'>View My Work</a> */}
       </div>
     </section>
   );
